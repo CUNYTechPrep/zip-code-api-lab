37a1bc2c1172f787acadafaa073110f2b21c810d
PZDuck <agp2281337@gmail.com>
2020-10-16 20:04:00 -0400

    completed api lab

--- zip-api/app.js
+++ zip-api/app.js
@@ -1,28 +1,61 @@
-const express = require('express');
+const { query } = require("express");
+const express = require("express");
 const app = express();
+const { capitalize } = require("./utils");
 
-const zipdb = require('./zipData');
+const zipdb = require("./zipData");
 
 const PORT = process.env.PORT || 8000;
 
-
-// console.log(zipdb.byCity);
-
-
-app.get('/', (req, res) => {
-  res.json({test: 'Yay'});
+app.get("/", (req, res) => {
+  res.json({ test: "Yay" });
 });
 
+app.get("/zip/:zipcode", (req, res) => {
+  const queryparams = req.query;
 
-app.get('/zip/:zipcode', (req, res) => {
-  // fill in...
+  const zipcode = req.params.zipcode;
+
+  const response = zipdb.byZip[zipcode];
+
+  try {
+    if (Object.keys(queryparams).length) {
+      const filteredRes = [];
+      for (let zip of response) {
+        if (
+          Object.keys(queryparams).every(
+            (q) => zip[capitalize(q)] == queryparams[q]
+          )
+        )
+          filteredRes.push(zip);
+      }
+
+      res.json(filteredRes);
+    } else {
+      res.json(response);
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
+app.get("/city/:cityname", (req, res) => {
+  const cityname = req.params.cityname;
 
-app.get('/city/:cityname', (req, res) => {
-  // fill in...
+  const response = zipdb.byCity[cityname.toUpperCase()];
+
+  try {
+    const newResponse = {};
+
+    for (let zip of response) {
+      newResponse[zip] = zipdb.byZip[zip];
+    }
+
+    res.json(newResponse);
+  } catch (err) {
+    console.log(err);
+  }
 });
-
 
 app.listen(PORT, () => {
   console.log(`zip-api is up and running on ${PORT}`);
