3a7e4010a5a85ca25cbf876f338354c7805cb4c2
darienmiller88 <darienmiller88@yahoo.com>
2020-10-21 10:26:45 -0400

    initial commit

--- zip-api/app.js
+++ zip-api/app.js
@@ -4,17 +4,21 @@ const app = express();
 const zipdb = require('./zipData');
 
 const PORT = process.env.PORT || 8000;
+const ZIP_URL = "http://ctp-zip-api.herokuapp.com/zip";
+const CITY_URL = "http://ctp-zip-api.herokuapp.com/city";
 
 
 // console.log(zipdb.byCity);
 
+app.use(express.static("static"))
 
 app.get('/', (req, res) => {
   res.json({test: 'Yay'});
 });
 
-
 app.get('/zip/:zipcode', (req, res) => {
+   
+  res.status(200).send({zip: `Your zip: ${req.params.zipcode}`});
   // fill in...
 });
 
