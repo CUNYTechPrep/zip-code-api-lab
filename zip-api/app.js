02bdedb89e5485c83a24e5d9c51a2d78afea7ba2
Jephter Maurice <mjephter2@gmail.com>
2021-03-18 17:34:37 -0400

    Implementated zip API for search by city name and for seach by zipcode

--- zip-api/app.js
+++ zip-api/app.js
@@ -1,28 +1,32 @@
-const express = require('express');
+const { json, response } = require("express");
+const express = require("express");
 const app = express();
 
-const zipdb = require('./zipData');
+app.use(express.json());
+
+const { byZip, byCity } = require("./zipData");
 
 const PORT = process.env.PORT || 8000;
 
-
 // console.log(zipdb.byCity);
 
-
-app.get('/', (req, res) => {
-  res.json({test: 'Yay'});
+app.get("/", (req, res) => {
+  res.json({ test: "Yay" });
 });
 
-
-app.get('/zip/:zipcode', (req, res) => {
+app.get("/zip/:zipcode", (req, res) => {
   // fill in...
+  const zip = req.params.zipcode;
+  //console.log(zip);
+  res.json(byZip[zip]);
 });
 
-
-app.get('/city/:cityname', (req, res) => {
+app.get("/city/:cityname", (req, res) => {
   // fill in...
+  const ctname = req.params.cityname.toUpperCase();
+  //console.log(ctname);
+  res.json(byCity[ctname]);
 });
-
 
 app.listen(PORT, () => {
   console.log(`zip-api is up and running on ${PORT}`);
