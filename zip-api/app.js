e2be7b7f4ea0497b3dc00d0868efb27dd0dd5d62
Xiangmin Mo <xiangminmo6121@gmail.com>
2021-03-17 20:27:48 -0400

    filled in city and zip get routes

--- zip-api/app.js
+++ zip-api/app.js
@@ -5,22 +5,45 @@ const zipdb = require('./zipData');
 
 const PORT = process.env.PORT || 8000;
 
-
+// console.log(zipdb.byZip);
 // console.log(zipdb.byCity);
 
-
 app.get('/', (req, res) => {
-  res.json({test: 'Yay'});
+  res.json({ test: 'Yay' });
 });
 
 
 app.get('/zip/:zipcode', (req, res) => {
   // fill in...
+  const zip = req.params.zipcode;
+  const result = zipdb.byZip[zip];
+  try {
+    if (result !== undefined) {
+      res.status(200).json(result);
+    }
+    else{
+      res.status(500).send('Invalid Zip');
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 
 app.get('/city/:cityname', (req, res) => {
   // fill in...
+  const city = req.params.cityname.toUpperCase();
+  const result = zipdb.byCity[city];
+  try {
+    if (result !== undefined) {
+      res.status(200).json(result);
+    }
+    else{
+      res.status(500).send('City not found');
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 
