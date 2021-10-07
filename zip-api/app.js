545ad426725355ec5cb432d85ae1e783d340f5b3
WENKAITAN <wenkaitan1019@gmail.com>
2021-10-07 15:58:12 -0400

    backend 1

--- zip-api/app.js
+++ zip-api/app.js
@@ -6,7 +6,7 @@ const zipdb = require('./zipData');
 const PORT = process.env.PORT || 8000;
 
 
-// console.log(zipdb.byCity);
+// console.log(zipdb.byZip);
 
 
 app.get('/', (req, res) => {
@@ -16,11 +16,16 @@ app.get('/', (req, res) => {
 
 app.get('/zip/:zipcode', (req, res) => {
   // fill in...
+  const zipcode = req.params.zipcode
+  const result = zipdb.byZip[zipcode];
+  return res.send(result)
 });
 
 
 app.get('/city/:cityname', (req, res) => {
   // fill in...
+  const citynames = req.params.cityname
+  return res.json(zipdb.byCity[citynames])
 });
 
 
