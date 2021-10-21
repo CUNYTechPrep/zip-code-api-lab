94230a6082f127ea6bdfea39372597d937ff6c95
Farukh Khassan Khojayev <farukh022198@gmail.com>
2021-10-20 23:43:10 -0400

    zip-code-api-lab Instructor: Edgardo TA: Silvena

--- zip-api/app.js
+++ zip-api/app.js
@@ -6,24 +6,53 @@ const zipdb = require('./zipData');
 const PORT = process.env.PORT || 8000;
 
 
-// console.log(zipdb.byCity);
 
 
 app.get('/', (req, res) => {
-  res.json({test: 'Yay'});
+  res.json({ test: 'Yay' });
 });
 
 
-app.get('/zip/:zipcode', (req, res) => {
-  // fill in...
-});
+
+
 
 
 app.get('/city/:cityname', (req, res) => {
-  // fill in...
+  const cities = req.params.cityname.toUpperCase();
+
+
+  if (/^[a-zA-Z]+$/.test(cities)) {
+
+    const results = zipdb.byCity[cities];
+    if (results === undefined) {
+      res.json("city not found!");
+    }
+    res.json(results);
+  } else {
+    res.json("city not found!");
+  }
 });
 
 
 app.listen(PORT, () => {
-  console.log(`zip-api is up and running on ${PORT}`);
+  console.log(`zip-api is up and running on http://localhost:${PORT}`);
+});
+
+app.get('/zip/:zipcode', (req, res) => {
+  const zipcode = req.params.zipcode;
+
+
+  if (zipcode.length === 5) {
+
+    const results = zipdb.byZip[zipcode];
+    if (results === undefined) {
+      res.json("zip not found!");
+    }
+    res.json(results);
+  } else {
+    res.json("zip not found!");
+  }
+
+
+
 });
