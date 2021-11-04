56325ef17db62cd09832b86f69ff02e7d5f8d452
wenleeqc <wenleeqc@gmail.com>
2021-11-04 17:21:50 -0400

    finish zip code api

--- zip-api/app.js
+++ zip-api/app.js
@@ -13,16 +13,31 @@ app.get('/', (req, res) => {
   res.json({test: 'Yay'});
 });
 
-
 app.get('/zip/:zipcode', (req, res) => {
-  // fill in...
-});
+  // get city data from zipData
+  const zips = zipdb.byZip[req.params.zipcode];
 
+  // if zipcodes exist, return city data
+  // else return 'not found' status code
+  if(zips) {
+    res.json(zips);
+  } else {
+    res.sendStatus('404');
+  }
+});
 
 app.get('/city/:cityname', (req, res) => {
-  // fill in...
-});
+  // get zipcode data from zipData
+  const cities = zipdb.byCity[req.params.cityname.toUpperCase()];
 
+  // if city exist, return zipcode info
+  // else return 'not found' status code
+  if(cities) {
+    res.json(cities);
+  } else {
+    res.sendStatus('404');
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`zip-api is up and running on ${PORT}`);
