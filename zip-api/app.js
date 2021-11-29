2d315380d0ed14d0ed850b6ae3ac3ab52b900df1
kang lin <kanglin104@gmail.com>
2021-11-29 15:27:32 -0500

    express lab

--- zip-api/app.js
+++ zip-api/app.js
@@ -9,19 +9,20 @@ const PORT = process.env.PORT || 8000;
 // console.log(zipdb.byCity);
 
 
-app.get('/', (req, res) => {
-  res.json({test: 'Yay'});
+app.get("/", (req, res) => {
+  res.json({ test: "Yay" });
 });
 
-
-app.get('/zip/:zipcode', (req, res) => {
-  // fill in...
+app.get("/zip/:zipcode", (req, res) => {
+  const zipcode = req.params.zipcode;
+  res.staus(200).json(zipdb.byZip[zipcode]);
 });
 
-
-app.get('/city/:cityname', (req, res) => {
-  // fill in...
+app.get("/city/:cityname", (req, res) => {
+  const cityname = req.params.cityname;
+  res.staus(200).json(zipdb.byCity[cityname]);
 });
+
 
 
 app.listen(PORT, () => {
