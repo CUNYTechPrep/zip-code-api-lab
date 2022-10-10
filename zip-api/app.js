aa9eb57d1bf16f1cbe33fe960e21b379e4ba2fa8
Ahmai Chaney-Smith <ahmai.chaney-smith@macaulay.cuny.edu>
2022-10-10 18:22:41 -0400

    assignment completed

--- zip-api/app.js
+++ zip-api/app.js
@@ -5,24 +5,31 @@ const zipdb = require('./zipData');
 
 const PORT = process.env.PORT || 8000;
 
-
-// console.log(zipdb.byCity);
-
-
 app.get('/', (req, res) => {
   res.json({test: 'Yay'});
 });
 
-
 app.get('/zip/:zipcode', (req, res) => {
-  // fill in...
+  let theZip = req.params.zipcode;
+  let theObj = zipdb.byZip[theZip];
+  
+  if(theObj !== undefined){
+    res.json(theObj);
+  } else{
+    res.json("Not a valid zipcode");
+  }
 });
-
 
 app.get('/city/:cityname', (req, res) => {
-  // fill in...
+  let theCity = req.params.cityname;
+  let theObj = zipdb.byCity[theCity];
+  
+  if(theObj !== undefined){
+    res.json(theObj);
+  } else{
+    res.json("Not a valid cityname");
+  }
 });
-
 
 app.listen(PORT, () => {
   console.log(`zip-api is up and running on ${PORT}`);
