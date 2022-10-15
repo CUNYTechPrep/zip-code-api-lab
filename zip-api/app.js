b325567d63ee47c6322093c04b16aa018ae6fc96
Jaichyi Chen <jaichyichen@gmail.com>
2022-10-15 18:56:14 -0400

    finished lab

--- zip-api/app.js
+++ zip-api/app.js
@@ -1,28 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const zipdb = require('./zipData');
+const zipdb = require("./zipData");
 
 const PORT = process.env.PORT || 8000;
 
-
 // console.log(zipdb.byCity);
 
-
-app.get('/', (req, res) => {
-  res.json({test: 'Yay'});
+app.get("/", (req, res) => {
+  res.json({ test: "Yay" });
 });
 
-
-app.get('/zip/:zipcode', (req, res) => {
-  // fill in...
+app.get("/zip/:zipcode", (req, res) => {
+  const zipcode = req.params.zipcode;
+  res.json(zipdb.byZip[zipcode]);
 });
 
-
-app.get('/city/:cityname', (req, res) => {
-  // fill in...
+app.get("/city/:cityname", (req, res) => {
+  const cityName = req.params.cityname;
+  res.json(zipdb.byCity[cityName]);
 });
-
 
 app.listen(PORT, () => {
   console.log(`zip-api is up and running on ${PORT}`);
