9712c7d67a6612f43b7c484e517d335cdda0c2f5
Tianye Chen <78762926+tianye-chen@users.noreply.github.com>
2022-10-22 16:42:02 -0400

    complete

--- zip-api/app.js
+++ zip-api/app.js
@@ -6,21 +6,22 @@ const zipdb = require('./zipData');
 const PORT = process.env.PORT || 8000;
 
 
-// console.log(zipdb.byCity);
+//console.log(zipdb.byCity);
 
-
-app.get('/', (req, res) => {
-  res.json({test: 'Yay'});
+app.get("/", (req, res) => {
+    res.json({ test: "Yaaa" });
 });
 
 
 app.get('/zip/:zipcode', (req, res) => {
-  // fill in...
+  const zipcode = req.params.zipcode
+  res.json(zipdb.byZip[zipcode] ? zipdb.byZip[zipcode] : "Not Found");
 });
 
 
 app.get('/city/:cityname', (req, res) => {
-  // fill in...
+  const city = req.params.cityname.toUpperCase();
+  res.json(zipdb.byCity[city] ? zipdb.byCity[city] : "Not Found");
 });
 
 
